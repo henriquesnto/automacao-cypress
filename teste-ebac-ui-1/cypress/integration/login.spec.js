f6736c2fcede8c51bf8c6df04648a8b978588c26
Luiz Santos <luiz.santos@bee2pay.com>
2024-03-13 08:22:47 -0300

    Criando Teste de Pre-Cadastro

--- teste-ebac-ui-1/cypress/integration/login.spec.js
+++ teste-ebac-ui-1/cypress/integration/login.spec.js
@@ -1,18 +1,25 @@
 /// <reference types="cypress" />
 
 context('Funcionalidade Login', () =>{
-   
-   it('Deve fazer login com sucesso', ()=>{
+
+   beforeEach(() => {
       cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+   });
+  
+   afterEach(()=>{
+      cy.screenshot()
+   })
+
+   it('Deve fazer login com sucesso', ()=>{
       cy.get('#username').type('aluno_ebac@teste.com')
       cy.get('#password').type('teste@teste.com')
       cy.get('.woocommerce-form > .button').click()
+
       cy.get('a > .hidden-xs').should('contain','Welcome aluno_ebac !')
 
    })
    
    it('Deve exibir uma mensagem de erro ao inserir usuario ou senha inválidos', () =>{
-      cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
       cy.get('#username').type('ebac@teste.com')
       cy.get('#password').type('teste@teste.com')
       cy.get('.woocommerce-form > .button').click()
@@ -21,7 +28,6 @@ context('Funcionalidade Login', () =>{
    })
 
    it('Deve exibir uma mensagem de erro ao inserir senha inválidos', () =>{
-      cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
       cy.get('#username').type('aluno_ebac@teste.com')
       cy.get('#password').type('teste@teste')
       cy.get('.woocommerce-form > .button').click()
