10f23d28eb81d61264cdb7a072f404703a1318f9
henriquesnto <henriquesnto@outlook.com>
2024-03-13 05:21:38 -0300

    Criando cenarios invalidos

--- teste-ebac-ui-1/cypress/integration/login.spec.js
+++ teste-ebac-ui-1/cypress/integration/login.spec.js
@@ -12,7 +12,22 @@ context('Funcionalidade Login', () =>{
    })
    
    it('Deve exibir uma mensagem de erro ao inserir usuario ou senha inválidos', () =>{
+      cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+      cy.get('#username').type('ebac@teste.com')
+      cy.get('#password').type('teste@teste.com')
+      cy.get('.woocommerce-form > .button').click()
 
+      cy.get('.woocommerce-error > li').should('contain','Endereço de e-mail desconhecido.')
    })
+
+   it('Deve exibir uma mensagem de erro ao inserir senha inválidos', () =>{
+      cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+      cy.get('#username').type('aluno_ebac@teste.com')
+      cy.get('#password').type('teste@teste')
+      cy.get('.woocommerce-form > .button').click()
+
+      cy.get('.woocommerce-error > li').should('contain','Erro: A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta.')
+   })
+   
 
 })
